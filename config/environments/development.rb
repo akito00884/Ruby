bee04d16a0f49493fa369c0d6cccf0af14d442cd
西村 彰人 <nishimura_a@nishimura-no-MacBook-Air.local>
2014-04-06 21:30:37 +0900

    first commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,17 +1,17 @@
 Blog::Application.configure do
 
 　# メール送信設定
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-  :enable_starttls_auto => true,
-  :address => 'smtp.gmail.com',
-  :port => '587',
-  :domain => 'smtp.gmail.com',
-  :authentication => 'plain',
-  :user_name => 'ユーザー名@gmail.com',
-  :password => 'gmailパスワード'
-  }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :domain => 'smtp.gmail.com',
+    :authentication => 'plain',
+    :user_name => 'ユーザー名@gmail.com',
+    :password => 'gmailパスワード'
+    }
 
   # Settings specified here will take precedence over those in config/application.rb
 
